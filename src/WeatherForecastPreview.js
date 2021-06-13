b45c9abfb45997592a4abcabd6d84d3d56416782
natalieh93 <natalieh93@users.github.com>
2021-06-13 16:31:29 +0200

    Used the OneCall API for weather forecast

--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
-export default function WeatherForecastPreview() {
+import WeatherForecastDay from "./WeatherForecastDay.js"
+
+export default function WeatherForecastPreview(props) {
+
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+  
+  useEffect (() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response){
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
   return (
     <div className="WeatherForecast">
-      <div className="row"></div>
+      <div className="row">   {forecast.map (function (dailyForecast, index) {
+          if (index < 5) {
+            return (
+             <div className="col" key={index}>
+               <WeatherForecastDay data={dailyForecast} />
+            </div>
+            );
+          } else {
+            return null;
+          }
+        })}</div>
     </div>
   );
+} else {
+    let apiKey = "1fa9ff4126d95b8db54f3897a208e91c";
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading..."
+  }
 }
